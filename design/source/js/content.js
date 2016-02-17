1c527e3a97a0cfc95c524a29dcc8313bf6348f48
marcus8712 <marcus@hoiio.com>
2016-02-17 15:21:09 +0700

    version 0.1.0

--- design/source/js/content.js
+++ design/source/js/content.js
@@ -20,7 +20,7 @@
     hoiio += "      <nav id='left-content-sidebar'>";
     hoiio += "<ul class=\"category\">";
     hoiio += "    <li class=\"title\">Getting Started<\/li>";
-    hoiio += "    <li class=\"item\"><a href=\"\/design\introduction\">Introduction<\/a><\/li>";
+    hoiio += "    <li class=\"item\"><a href=\"\/design\/introduction\">Introduction<\/a><\/li>";
     hoiio += "    <li class=\"item line through\"><a href=\"\/design\/usage\/prototype\">Prototype<\/a><\/li>";
     hoiio += "    <li class=\"item line through\"><a href=\"\/design\/usage\/samples\">Samples<\/a><\/li>";
     hoiio += "<\/ul>";
